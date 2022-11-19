bc5d5c2cbdfc3c5727352486fb248521ba92fbad
なかとも <20672947+naka-tomo@users.noreply.github.com>
2022-11-19 14:40:22 +0900

    声調整

--- bin/openjtalk.sh
+++ bin/openjtalk.sh
@@ -1,7 +1,10 @@
 OPENJTALK_DIR=/home/root/openjtalk
-VOICE=$OPENJTALK_DIR/MMDAgent_Example-1.4/Voice/mei/mei_normal.htsvoice
+VOICE=$OPENJTALK_DIR/MMDAgent_Example-1.4/Voice/mei/mei_happy.htsvoice
 DIC=$OPENJTALK_DIR/open_jtalk_dic_utf_8-1.08
 echo $1 | open_jtalk \
+-a 0.51 \
+-fm -1.0 \
+-jf 1.2 \
 -m  $VOICE \
 -x  $DIC \
 -ow $2
